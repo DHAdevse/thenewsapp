0c8281d49a660181c0b00092b8497ef042ab2e9d
DHAdevse <21110751@student.hcmute.edu.vn>
2024-05-31 09:34:22 +0700

    my commit

---
+++ app/src/main/java/com/example/thenewsappmobile/ChatUiEvent.kt
@@ -0,0 +1,16 @@
+package com.ahmedapps.thenewsapp
+
+import android.graphics.Bitmap
+/*
+Person Responsible for this class : DINH HUU QUANG ANH - 21110752
+*/
+
+sealed class ChatUiEvent {
+    //The purpose of this class is likely to represent an event where the prompt in the UI needs to be updated with a new value.
+    data class UpdatePrompt(val newPrompt: String) : ChatUiEvent()
+    //The prompt with an image (bitmap), needs to be sent or processed.
+    data class SendPrompt(
+        val prompt: String,
+        val bitmap: Bitmap?
+    ) : ChatUiEvent()
+}
